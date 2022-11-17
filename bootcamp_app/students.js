e9f892128d6a44e12b3dc95d8834dec2bf66a50b
Tuigun Bekishev <tbekishev@gmail.com>
2022-11-16 21:55:06 -0400

    add parameterized query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`, limit];
+
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
@@ -11,11 +15,10 @@ pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, values)
 .then(res => {
-  console.log(res.rows)
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,7 @@
 const { Pool } = require('pg');
 
+const values = [process.argv[2]];
+
 const pool = new Pool({
   user: 'labber',
   password: 'labber',
@@ -13,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2]}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, values)
 .then(res => {
   res.rows.forEach(key => {
     console.log(`${key.cohort}: ${key.teacher}`);
